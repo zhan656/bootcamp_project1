5eb4c8920647a39e8123646f91b912553bab6a4b
zhan656 <34688799+zhan656@users.noreply.github.com>
2018-04-01 14:17:07 -0700

    comments added

--- config.py
+++ config.py
@@ -1,2 +1,1 @@
 api_key="uNLZVdQa8fGgJ74oK3sy"
-fred_api = '3018accf605e4481d146ad26816635a8'
--- ploting_headmaps.py
+++ ploting_headmaps.py
@@ -4,38 +4,31 @@ from config import api_key
 import plotly
 import plotly.plotly as py
 class main():
-    # #import plotly
-    # #import plotly.plotly as py
-    # import pandas as pd
-    # #from config import api_key
-    # import json
-    # from states_trans import states
     
     def per_year(data_all_years,state_pop, year):
+        # Sum up all flu cases and flu percent in each state in given year
         df_state1 = data_all_years.loc[data_all_years['year']==year,:]
         df_grouped_flu = df_state1.groupby(['state']).sum()
         df_grouped_flu = pd.DataFrame(df_grouped_flu[['flu_cases','flu_percent']])
         # sum_cases = df_grouped_flu['flu_cases'].sum()
         # percent_flu = [(i/sum_cases)*100 for i in df_grouped_flu['flu_cases']]
         # df_grouped_flu['flu_percent'] = percent_flu
-        # Accumulated Vaccination Rate and Cases in each state
+       
+       # Accumulated Vaccination Rate and Cases in each state
         df_grouped_vac = df_state1.loc[df_state1['week']==52,['state','vac_percent','vaccinations']]
         df_grouped_vac.head()
         
         # Create a State Abbreviation table
-
-        #code = [states[i] for i in df_plot['state'].unique()]
         state_name_df = pd.DataFrame({"state":df_state1['state'].unique()})
         state_name_df['code'] = [states[i] for i in state_name_df['state']]
         state_name_df.head()
         
         # Add corresponding State code to grouped flu table
-        #abbrv = list(state_name_df['code'])
         df_grouped_flu['code'] = list(state_name_df['code'])
         df_grouped_flu['code'] =df_grouped_flu['code'].astype(str)
         df_grouped_flu.tail(10)
         
-        # Add coresponding State code to grouped flu table
+        # Add corresponding State code to grouped flu table
         df_grouped_vac['code'] = list(state_name_df['code'])
         df_grouped_vac['code'] = df_grouped_vac['code'].astype(str)
         df_grouped_vac.head(10)
@@ -48,9 +41,6 @@ class main():
     
     
     def plot_heatmaps(df_flu_vac,year):
-        # from config import api_key
-        # import plotly
-        # import plotly.plotly as py
         
         # API key to 'Plotly'
         plotly.tools.set_credentials_file(username='zhan656', api_key=api_key)
@@ -70,15 +60,17 @@ class main():
            [0.3, 'rgb(77, 198, 255)'],[0.4, 'rgb(26, 182, 255)'],[0.5, 'rgb(0, 157, 230)'],\
             [0.6, 'rgb(0, 139, 204)'],[0.7, 'rgb(0, 122, 179)'], [0.8, 'rgb(0, 105, 153)'],[0.9, 'rgb(0, 87, 128)'],\
            [1.0, 'rgb(0, 70, 102)']]
-           
+        
+        # text in flu heatmap
         df_flu_vac['flu_text'] = df_flu_vac['code'] + '<br>' +\
                     "Flu Percentage: " + df_flu_vac['flu_percent'] + '<br>' +\
                     "Flu Cases: " + df_flu_vac['flu_cases']
-        
+        # text in vac heatmap
         df_flu_vac['vac_text'] = df_flu_vac['code'] + '<br>' +\
                     "Vaccination Percentage: " + df_flu_vac['vac_percent'] + '<br>' +\
                     "Vaccination Cases: " + df_flu_vac['vaccinations']
         
+        # Data in flu heatmaps
         data_flu = [ dict(
                     type='choropleth',
                     colorscale = scl_flu,
@@ -95,7 +87,8 @@ class main():
                     colorbar = dict(
                         title = "Flu Percentage")
                     ) ]
-                    
+        
+        # Data in vac heatmaps
         data_vac = [ dict(
                     type='choropleth',
                     colorscale = scl_vac,
@@ -112,7 +105,8 @@ class main():
                     colorbar = dict(
                         title = "Vaccination Percentage")
                     ) ]
-                    
+         
+        # layout in flu heatmaps
         layout_flu = dict(
                 title = '<b>Flu Percentage by State Heatmap {} - {}<b>'.format(str(year),str(year+1)),
                 font = dict(size=18, color='rgb(0,0,0)'),
@@ -124,6 +118,7 @@ class main():
                
                      )
         
+        # layout in vac heatmaps
         layout_vac = dict(
                 title = '<b>Vaccination Percentage by State Heatmap {} - {}<b>'.format(str(year),str(year+1)),
                 font = dict(size=18, color='rgb(0,0,0)'),
@@ -134,7 +129,7 @@ class main():
                     lakecolor = 'rgb(255, 255, 255)'),
             
 )
-
+        # plotting 'flu' and 'vac'
         fig_flu = dict( data=data_flu, layout=layout_flu )
         url = py.plot( fig_flu, filename='flu_map'+str(year))
         fig_vac = dict( data=data_vac, layout=layout_vac)
